e86a7c19a3d43e806f4889d42d5b74ab4f2edb56
hxgf <jonathan.youngblood@gmail.com>
2017-03-15 15:40:33 -0500

    parse string method, updated email_send error handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 module.exports = function(app){
+
   var module = {};
 
   // returns a date a given number of days from a given date
@@ -89,18 +90,15 @@ module.exports = function(app){
       mail_options.context = params.data;
     }
     $.mail_transport.sendMail(mail_options, function (err, info) {
-      if (err) {
-        $.lexxi.application_error({
+      if (err) return $.lexxi.application_error({
           type: 'email_send',
           err: err
         });
-        return next(err);
-      }
-      if (info){
-        console.log(info);
-      }
+      // if (info){
+      //   console.log(info);
+      // }
     });
-  	return true;
+    return true;
   };
 
 
@@ -258,6 +256,93 @@ module.exports = function(app){
     };
     return _unserialize((data + ''), 0)[2];
   };
+
+
+
+
+  module.parse_str = function(str, array) { // this, too, is from php.js
+    var strArr = String(str)
+      .replace(/^&/, '')
+      .replace(/&$/, '')
+      .split('&'),
+      sal = strArr.length,
+      i, j, ct, p, lastObj, obj, lastIter, undef, chr, tmp, key, value,
+      postLeftBracketPos, keys, keysLen,
+      fixStr = function(str) {
+        return decodeURIComponent(str.replace(/\+/g, '%20'));
+      };
+    if (!array) {
+      array = this.window;
+    }
+    for (i = 0; i < sal; i++) {
+      tmp = strArr[i].split('=');
+      key = fixStr(tmp[0]);
+      value = (tmp.length < 2) ? '' : fixStr(tmp[1]);
+      while (key.charAt(0) === ' ') {
+        key = key.slice(1);
+      }
+      if (key.indexOf('\x00') > -1) {
+        key = key.slice(0, key.indexOf('\x00'));
+      }
+      if (key && key.charAt(0) !== '[') {
+        keys = [];
+        postLeftBracketPos = 0;
+        for (j = 0; j < key.length; j++) {
+          if (key.charAt(j) === '[' && !postLeftBracketPos) {
+            postLeftBracketPos = j + 1;
+          } else if (key.charAt(j) === ']') {
+            if (postLeftBracketPos) {
+              if (!keys.length) {
+                keys.push(key.slice(0, postLeftBracketPos - 1));
+              }
+              keys.push(key.substr(postLeftBracketPos, j - postLeftBracketPos));
+              postLeftBracketPos = 0;
+              if (key.charAt(j + 1) !== '[') {
+                break;
+              }
+            }
+          }
+        }
+        if (!keys.length) {
+          keys = [key];
+        }
+        for (j = 0; j < keys[0].length; j++) {
+          chr = keys[0].charAt(j);
+          if (chr === ' ' || chr === '.' || chr === '[') {
+            keys[0] = keys[0].substr(0, j) + '_' + keys[0].substr(j + 1);
+          }
+          if (chr === '[') {
+            break;
+          }
+        }
+        obj = array;
+        for (j = 0, keysLen = keys.length; j < keysLen; j++) {
+          key = keys[j].replace(/^['"]/, '')
+            .replace(/['"]$/, '');
+          lastIter = j !== keys.length - 1;
+          lastObj = obj;
+          if ((key !== '' && key !== ' ') || j === 0) {
+            if (obj[key] === undef) {
+              obj[key] = {};
+            }
+            obj = obj[key];
+          } else { // To insert new dimension
+            ct = -1;
+            for (p in obj) {
+              if (obj.hasOwnProperty(p)) {
+                if (+p > ct && p.match(/^\d+$/g)) {
+                  ct = +p;
+                }
+              }
+            }
+            key = ct + 1;
+          }
+        }
+        lastObj[key] = value;
+      }
+    }
+  };
+
 
 
 
